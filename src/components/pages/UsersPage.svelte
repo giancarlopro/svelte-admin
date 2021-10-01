b06b2af62ab2688721c907826c690bad6221d53e
Giancarlo Rocha <giancarloiff@gmail.com>
2021-10-01 11:10:37 -0300

    feat: add users table

--- src/components/pages/UsersPage.svelte
+++ src/components/pages/UsersPage.svelte
@@ -3,9 +3,12 @@ import Main from '../atoms/Main.svelte';
 import Navbar from '../organisms/Navbar.svelte';
 
 import Sidebar from '../organisms/Sidebar.svelte';
+import UsersTable from '../organisms/users/UsersTable.svelte';
 </script>
 
 <Sidebar active='usuarios' />
 <Main>
   <Navbar title="Usuáros" />
+
+  <UsersTable />
 </Main>
