4cd2bc30706fd9c1f8519a45c64af54db18410cf
Giancarlo Rocha <giancarloiff@gmail.com>
2021-10-03 15:03:10 -0300

    feat: add dynamic navbar breadcrumbs

--- src/components/organisms/Navbar.svelte
+++ src/components/organisms/Navbar.svelte
@@ -1,5 +1,27 @@
 <script>
-  export let title = '';
+  import { page } from '$app/stores';
+
+  export let paths = [];
+
+  let labels = {
+    users: 'Usuários',
+    ticket_gates: 'Catracas',
+  };
+
+  $: paths = $page.path.split('/').filter(p => p.length !== 0);
+  $: currentPath = paths[paths.length - 1];
+
+  function labelFor(path) {
+    if (!path) return 'Dashboard';
+
+    return labels[path] || path.charAt(0).toUpperCase() + path.slice(1);
+  }
+
+  function linkFor(pathname) {
+    const index = paths.indexOf(pathname);
+
+    return paths.slice(0, index).join('/');
+  }
 </script>
 
 <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
@@ -7,9 +29,14 @@
     <nav aria-label="breadcrumb">
       <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
         <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="/">Dashboard</a></li>
-        <li class="breadcrumb-item text-sm text-dark active" aria-current="page">{title}</li>
+        {#each paths.slice(0, paths.length - 1) as pathname}
+          <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="{linkFor(pathname)}">{labelFor(pathname)}</a></li>
+        {/each}
+        {#if paths.length > 0}
+          <li class="breadcrumb-item text-sm text-dark active" aria-current="page">{labelFor(currentPath)}</li>
+        {/if}
       </ol>
-      <h6 class="font-weight-bolder mb-0">{title}</h6>
+      <h6 class="font-weight-bolder mb-0">{labelFor(currentPath)}</h6>
     </nav>
     <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
       <div class="ms-md-auto pe-md-3 d-flex align-items-center">
